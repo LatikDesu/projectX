c8c16ba279cc2ffc0ef13a354884d0ea688b4330
LatikDesu <evga.abrosimov@gmail.com>
2023-10-21 19:22:55 +0300

    test config

--- server/scripts/entrypoint.sh
+++ server/scripts/entrypoint.sh
@@ -2,22 +2,23 @@
 
 APP_PORT=${PORT:-8000}
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
+# Проверяем, были ли миграции выполнены
+if [ ! -f /app/server/scripts/migrations_done ]; then
+    /app/server/scripts/migrations.sh
+    touch /app/server/scripts/migrations_done
 fi
 
-/app/server/scripts/migrations.sh
+# Проверяем, был ли createsuperuser выполнен
+if [ ! -f /app/server/scripts/createsuperuser_done ]; then
+    /app/server/scripts/createsuperuser.sh
+    touch /app/server/scripts/createsuperuser_done
+fi
 
-/app/server/scripts/createsuperuser.sh
-
-/app/server/scripts/loaddata.sh
+# Проверяем, было ли loaddata выполнено
+if [ ! -f /app/server/scripts/loaddata_done ]; then
+    /app/server/scripts/loaddata.sh
+    touch /app/server/scripts/loaddata_done
+fi
 
 /opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm server.wsgi:application --bind "0.0.0.0:${APP_PORT}"
 
