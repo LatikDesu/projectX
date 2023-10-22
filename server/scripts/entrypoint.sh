1b602edd832871df308431dff86cac8ba4d2972c
LatikDesu <evga.abrosimov@gmail.com>
2023-10-22 19:57:41 +0300

    logging

--- server/scripts/entrypoint.sh
+++ server/scripts/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+APP_HOST=${HOST:-0.0.0.0}
 APP_PORT=${PORT:-8000}
+LOG_LEVEL=${LOG_LEVEL:-info}
+LOG_CONFIG=${LOG_CONFIG:-/app/server/logging.ini}
+
+export WORKER_CLASS=${WORKER_CLASS:-"uvicorn.workers.UvicornWorker"}
+export APP_MODULE=${APP_MODULE:-"server.wsgi:application"}
+
+sleep 10
 
 # Проверяем, были ли миграции выполнены
 if [ ! -f /app/server/scripts/migrations_done ]; then
@@ -20,5 +28,4 @@ if [ ! -f /app/server/scripts/loaddata_done ]; then
     touch /app/server/scripts/loaddata_done
 fi
 
-/opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm server.wsgi:application --bind "0.0.0.0:${APP_PORT}"
-
+/opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm --bind "${APP_HOST}:${APP_PORT}" --log-config $LOG_CONFIG "$APP_MODULE"
