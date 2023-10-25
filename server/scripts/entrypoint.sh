3462ac490fe15a9b6228675d3ebe8062dd38d2d0
LatikDesu <evga.abrosimov@gmail.com>
2023-10-25 23:25:04 +0300

    EntryPoint

--- server/scripts/entrypoint.sh
+++ server/scripts/entrypoint.sh
@@ -10,4 +10,22 @@ export APP_MODULE=${APP_MODULE:-"server.wsgi:application"}
 
 sleep 10
 
+# Проверяем, были ли миграции выполнены
+if [ ! -f /app/server/scripts/migrations_done ]; then
+    /app/server/scripts/migrations.sh
+    touch /app/server/scripts/migrations_done
+fi
+
+# Проверяем, был ли createsuperuser выполнен
+if [ ! -f /app/server/scripts/createsuperuser_done ]; then
+    /app/server/scripts/createsuperuser.sh
+    touch /app/server/scripts/createsuperuser_done
+fi
+
+# Проверяем, было ли loaddata выполнено
+if [ ! -f /app/server/scripts/loaddata_done ]; then
+    /app/server/scripts/loaddata.sh
+    touch /app/server/scripts/loaddata_done
+fi
+
 /opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm --bind "${APP_HOST}:${APP_PORT}" --log-config $LOG_CONFIG "$APP_MODULE"
